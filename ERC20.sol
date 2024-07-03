32fb8e95679c1e4cdbb63e0df5960f0e09d35a25
MINI MONGA <121760856+Mansi-Monga@users.noreply.github.com>
2024-07-03 20:59:22 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,8 +1,31 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract SimpleToken is ERC20{
-construct() ERC("simple","SIMP"){
-_mint(msg.sender,100*10** decimals());
-}
+
+contract SimpleToken is ERC20 {
+    address public owner;
+
+    constructor() ERC20("simple", "SIMP") {
+        owner = msg.sender;
+        _mint(msg.sender, 100 * 10**decimals());
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
 }
